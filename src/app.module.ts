cbc1a3254af705c1ea3560dfb9f76b2954623fa7
Luis Torres <luis.torresgu@sansano.usm.cl>
2023-04-07 18:56:30 -0400

    established connection with mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ltorres661:Luisfelipe16@recylinkbbdd.74dqe7a.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
