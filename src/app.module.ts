b2332fb24413ff52ca9b2659763f68e79ceaba1b
Luis Torres <luis.torresgu@sansano.usm.cl>
2023-04-08 13:09:54 -0400

    conect to /graphql, pre change of module for new entitys

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MarcaModule } from './marca/marca.module';
 import { ModeloModule } from './modelo/modelo.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      driver: ApolloDriver,
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://ltorres661:Luisfelipe16@recylinkbbdd.74dqe7a.mongodb.net/?retryWrites=true&w=majority',
     ),
