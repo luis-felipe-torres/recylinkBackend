b925409be3b2d61cd61de7f4f2a0473069318dcd
Luis Torres <luis.torresgu@sansano.usm.cl>
2023-04-11 15:21:38 -0400

    hotfix docker networke bridge

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(
-    cors({
-      origin: 'http://localhost:3001',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      allowedHeaders: 'Content-Type, Accept, Authorization',
-    }),
-  );
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
 
   await app.listen(3000);
 }
